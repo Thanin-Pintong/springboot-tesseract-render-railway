c77552c94fc5e1d280e3d2a2e2efd9a0ae155245
Thanin-Pintong <thaninp@live.com>
2023-04-22 02:58:18 +0700

    first command

--- src/main/java/com/examples/basic/SpringbootBasicApplication.java
+++ src/main/java/com/examples/basic/SpringbootBasicApplication.java
@@ -1,12 +1,28 @@
 package com.examples.basic;
 
+import java.util.Locale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * To enable it run as WebApp, then you must extends SpringBootServletInitializer
+ */
 @SpringBootApplication
-public class SpringbootBasicApplication {
+public class SpringbootBasicApplication extends SpringBootServletInitializer {
+
+    /**
+     * Used in case of .war deployment that can run both in a container and from java -jar xxx.jar
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringbootBasicApplication.class);
+    }
 
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         SpringApplication.run(SpringbootBasicApplication.class, args);
     }
+    
 }
